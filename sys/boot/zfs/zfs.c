Tihauan/unix-history-repo

commit 131874691955243fbc90468b0b2003c70e89b51f

Avoid a double free in devopen by not freeing the device structure in zfs_dev_open.  This stops a panic in the loader when trying to read from a zfs device and no zfs devices exist.
git-svn-id: svn+ssh://svn.freebsd.org/base/head@185902 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/boot/zfs/zfs.c
@@ -474,7 +474,6 @@ zfs_dev_open(struct open_file *f, ...)
 	 * the pool.
 	 */
 	unit = dev->d_unit;
-	free(dev);
 	
 	i = 0;
 	STAILQ_FOREACH(spa, &zfs_pools, spa_link) {

commit 0129c7ef5a1d49c850cadb85006e8cd2067a19b0

Fix a leak introduced in r185902.  We should free the devspec if we've successfully found a zfs pool.

sys/boot/zfs/zfs.c
@@ -486,6 +486,7 @@ zfs_dev_open(struct open_file *f, ...)
 	}
 
 	f->f_devdata = spa;
+	free(dev);
 	return (0);
 }
 